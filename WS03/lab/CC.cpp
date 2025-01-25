223e4bd7830bb584024b21d216b2eb3d7717acb1
Yicheng Wang <wangyi1991cheng@gmail.com>
2025-01-25 11:54:51 -0500

    lab3 done

--- WS03/lab/CC.cpp
+++ WS03/lab/CC.cpp
@@ -1,3 +1,11 @@
+/**
+ * I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+ * Name :	Yicheng Wang
+ * Email :	ywang841@myseneca.ca
+ * Student ID :	150868206
+ * Date :	2025/1/25
+ */
+
 #include <iostream>
 #include "cstr.h"
 #include "CC.h"
@@ -37,5 +45,53 @@ namespace seneca {
    }
    
 bool CC::validate(const char* name, unsigned long long cardNo, short cvv, short expMon, short expYear) const {
+   return (strlen(name) > 2) &&
+          (cardNo >= 4000000000000000ull && cardNo <= 4099999999999999ull) &&
+          (cvv > 99 && cvv < 1000) &&
+          (expMon > 0 && expMon < 13) &&
+          (expYear > 23 && expYear < 33);
+}
+
+void CC::display() const{
+   if(isEmpty()){
+      cout << "Invalid Credit Card Record" << endl;
+   }
+   else{
+      display(name, number, expYear, expMon, cvv);
+   }
+}
+
+void CC::set(){
+   name = nullptr;
+   number = 0;
+   cvv = 0;
+   expMon = 0;
+   expYear = 0;
+}
+
+void CC::set(const char* cc_name, 
+         unsigned long long cc_no, 
+         short cvv, 
+         short expMon, 
+         short expYear){
+   cleanUp();
+   if(validate(cc_name, cc_no, cvv, expMon, expYear)){
+      if(!alocpy(name, cc_name)){
+         cout << "Memory Allocation Failed!" << endl;
+      }
+      number = cc_no;
+      this->cvv = cvv;
+      this->expMon = expMon;
+      this->expYear = expYear;
+   }
+}  
+
+void CC::cleanUp(){
+   freeMem(name);
+   set();
+}
+
+bool CC::isEmpty() const{
+   return (name == nullptr);
 }
 }
--- WS03/lab/CC.h
+++ WS03/lab/CC.h
@@ -19,7 +19,15 @@ namespace seneca {
       void prnNumber(unsigned long long no) const;
       bool validate(const char* name, unsigned long long cardNo, short cvv, short expMon, short expYear) const;
    public:
-   
+      void display() const;
+      void set();
+      void set(const char* cc_name, 
+         unsigned long long cc_no, 
+         short cvv, 
+         short expMon = 12, 
+         short expYear = 24);
+      void cleanUp();
+      bool isEmpty() const;
    
    };
 
